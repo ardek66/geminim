47ede2b0053a5cec0c801cf0efa02f71df92fa63
IDF <idf31@protonmail.com>
2020-08-16 23:58:52 +0300

    Initial commit

---
+++ geminim.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "IDF"
+description   = "A simple gemini server written in nim"
+license       = "GPL-3.0"
+srcDir        = "src"
+bin           = @["geminim"]
+
+
+# Dependencies
+
+requires "nim >= 1.3.5"
---
+++ src/geminim.nim
@@ -0,0 +1,98 @@
+import net, asyncnet, asyncdispatch, uri, os, mimetypes, strutils
+
+type Response = object
+  code: int
+  meta, body: string
+
+const
+  hostname = "localhost"
+  dir = "pub"
+
+let ctx = newContext(certFile="mycert.pem", keyFile="mykey.pem")
+var m = newMimeTypes()
+m.register(ext = "gemini", mimetype = "text/gemini")
+
+template linkFromPath(path: string): string =
+  "=> gemini://" & hostname / path
+
+proc serveFile(response: var Response, path: string) =
+  response.body = readFile(path)
+  if response.body.len > 0:
+    response.meta = m.getMimetype(path.splitFile.ext.toLowerAscii)
+  else:
+    response.body = "##<Empty File>"
+    response.meta = "text/gemini"
+
+proc serveDir(response: var Response, path: string) =
+  let relPath = path.relativePath(dir)
+  response.body.add "### Index of " & relPath & "\r\n"
+  if relPath.parentDir != "":
+    response.body.add linkFromPath(relPath.parentDir) & " [..]" & "\r\n"
+  
+  for kind, file in path.walkDir:
+    let uriPath = relativePath(file, dir, '/')
+    if uriPath.toLowerAscii == "index.gemini":
+      response.serveFile(file)
+      return
+    
+    response.body.add linkFromPath(uriPath) & ' ' & uriPath.extractFilename
+    case kind:
+    of pcFile: response.body.add " [FILE]"
+    of pcDir: response.body.add " [DIR]"
+    of pcLinkToFile, pcLinkToDir: response.body.add " [SYMLINK]"
+    response.body.add "\r\n"
+    
+  if response.body.len == 0: response.body = "Directory is empty"
+  response.meta = "text/gemini"
+
+proc parseRequest(client: AsyncSocket, line: string) {.async.} =
+  let res = parseUri(line)
+  var path = dir & res.path
+  if path.relativePath(dir).parentDir == "": path = dir
+
+  var response: Response
+
+  if res.hostname != hostname:
+    response.code = 53
+    response.meta = "PROXY NOT ALLOWED"
+  else:
+    response.code = 20
+    if fileExists(path):
+      response.serveFile(path)
+    elif dirExists(path):
+      response.serveDir(path)
+    else:
+      response.code = 51
+      response.meta = "'" & path & "' NOT FOUND"
+    
+    try:
+      await client.send($response.code & " " & response.meta & "\r\n")
+      if response.code == 20:
+        await client.send(response.body)
+    except:
+      let msg = getCurrentExceptionMsg()
+      await client.send("40 TEMP ERROR " & msg & "\r\n")
+
+proc handle(client: AsyncSocket) {.async.} =
+  let line = await client.recvLine()
+  echo line
+  if line.len == 0:
+    echo "client disconnected"
+    client.close()
+    return
+  await client.parseRequest(line)
+  client.close()
+
+proc serve() {.async.} =
+  var server = newAsyncSocket()
+  server.setSockOpt(OptReuseAddr, true)
+  server.setSockOpt(OptReusePort, true)
+  server.bindAddr(Port(1965))
+  server.listen()
+  ctx.wrapSocket(server)
+  while true:
+    let client = await server.accept()
+    ctx.wrapConnectedSocket(client, handshakeAsServer)
+    asyncCheck client.handle()
+
+waitFor serve()
