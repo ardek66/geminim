2945913000aa88a0b097db1192d4549cb8accf5c
IDF <idf31@protonmail.com>
2020-09-02 12:42:16 +0300

    Improved configuration

--- src/config.nim
+++ src/config.nim
@@ -1,4 +1,4 @@
-import parsecfg, strutils, strtabs, os
+import parsecfg, strutils, strtabs, os, streams
 
 type CgiConf = tuple
   dir, virtDir: string
@@ -15,28 +15,53 @@ const defaultHome =
   when defined(posix): "/home/$#/"
   else: "C:\\Users\\$#\\"
 
-proc get(dict: Config, value, default: string, section = ""): string =
-  result = dict.getSectionValue(section, value)
-  if result.len < 1: return default
-
 proc readSettings*(path: string): Settings =
-  let conf = loadConfig(path)
-
   result = Settings(
-    port: conf.get("port", "1965").parseInt,
-    certFile: conf.get("certFile", "mycert.pem"),
-    keyFile: conf.get("keyFile", "mykey.pem"),
+    port: 1965,
+    certFile: "mycert.pem",
+    keyFile: "mykey.pem",
     vhosts: newStringTable(modeCaseSensitive),
     redirects: newStringTable(modeCaseSensitive),
-    homeDir: conf.get("homeDir", defaultHome),
-    cgi: (dir: conf.get("dir", "cgi/", section = "CGI"),
-          virtDir: conf.get("virtDir", "", section = "CGI")))
+    homeDir: defaultHome,
+    cgi: (dir: "cgi/", virtDir: ""))
 
-  for rawHostname in conf.get("hostnames", "localhost").split(','):
-    let
-      hostname = rawHostname.strip
-      dir = conf.get("dir", hostname, section = hostname)
-      redirect = conf.get("redirect", "", section = hostname)
-    if redirect.len > 0: result.redirects[hostname] = redirect
-    elif dirExists(dir): result.vhosts[hostname] = dir
-    else: echo "Directory " & dir & " does not exist. Not adding ", hostname, " to hosts."
+  var f = newFilestream(path, fmRead)
+  if f != nil:
+    var p: CfgParser
+    p.open(f, path)
+
+    var section: string
+    while true:
+      var e = next(p)
+      case e.kind
+      of cfgEof: break
+      of cfgError: echo e.msg
+      of cfgSectionStart: section = e.section
+      of cfgKeyValuePair:
+        case section.toLowerAscii
+        of "":
+          case e.key.toLowerAscii
+          of "port": result.port = e.value.parseInt
+          of "certfile": result.certfile = e.value
+          of "keyfile": result.keyfile = e.value
+          of "homedir": result.homeDir = e.value
+        
+        of "vhosts":
+          if dirExists(e.value):
+            result.vhosts[e.key] = e.value
+          else:
+            echo e.value & " does not exist or is not a directory"
+            echo "Not adding " & e.key & " to hosts\n"
+          
+        of "redirects":
+          result.redirects[e.key] = e.value
+          
+        of "cgi":
+          case e.key.toLowerAscii
+          of "dir":
+            if dirExists(e.value): result.cgi.dir = e.value
+            else: echo "CGI directory " & e.value & " does not exist\n"
+          
+          of "virtdir": result.cgi.virtDir = e.value
+
+      else: discard
